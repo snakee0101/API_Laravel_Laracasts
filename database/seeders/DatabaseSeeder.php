f8cf50f22531dd22aa6547b0218859205b5c7b96
Danil <snakee0101@gmail.com>
2023-01-31 16:55:19 +0200

    prepared database, table, seeder and model (Lesson / Laracasts)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Lesson;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Lesson::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([ LessonsTableSeeder::class ]);
     }
 }
---
+++ database/seeders/LessonsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lesson;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class LessonsTableSeeder extends Seeder
+{
+    use WithFaker;
+
+    protected $faker;
+
+    public function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+    }
+
+    public function run()
+    {
+        foreach (range(1,30) as $index)
+        {
+            Lesson::create([
+                'title' => $this->faker->sentence,
+                'body' => $this->faker->paragraph
+            ]);
+        }
+    }
+}
