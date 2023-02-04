092fe01bd72540269090a0c35c3b77b2a039049b
Danil <snakee0101@gmail.com>
2023-02-04 16:46:45 +0200

    all tags, that were attached to resource, could be returned. Not found exception is transformed into json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +45,12 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Resource is not found'
+                ]
+            ], 404);
         });
     }
 }
--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TagResource;
+use App\Models\Lesson;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    public function index()
+    public function index(Lesson $lesson)
     {
-        //
+        return TagResource::collection( $lesson->tags );
     }
 
     public function create()
